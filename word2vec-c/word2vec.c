3f6ef2d9c2c87d8ead9e329f1d164a7a8333208c
skTim <lhythu@gmail.com>
2015-05-10 16:23:25 +0800

    commit on NLP node

--- word2vec-c/word2vec.c
+++ word2vec-c/word2vec.c
@@ -349,8 +349,10 @@ void InitNet() {
 	if (negative>0) {
 		a = posix_memalign((void **)&syn1neg, 128, (long long)vocab_size * layer1_size * sizeof(real));
 		if (syn1neg == NULL) {printf("Memory allocation failed\n"); exit(1);}
-		for (a = 0; a < vocab_size; a++) for (b = 0; b < layer1_size; b++)
-			syn1neg[a * layer1_size + b] = 0;
+		for (a = 0; a < vocab_size; a++) for (b = 0; b < layer1_size; b++){
+			next_random = next_random * (unsigned long long)25214903917 + 11;
+			syn1neg[a * layer1_size + b] = fabs((((next_random & 0xFFFF) / (real)65536) - 0.5) / layer1_size);
+		}
 	}
 	for (a = 0; a < vocab_size; a++) for (b = 0; b < layer1_size; b++) {
 		next_random = next_random * (unsigned long long)25214903917 + 11;
